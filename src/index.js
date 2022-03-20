476aaea05723c1ec3604838e19245f6fcff07eb3
Josh-Gotro <joshuagauthreaux@gmail.com>
2022-03-20 13:48:55 -0500

    event handler implemented

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ const token = process.env.TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-  console.log('OK BRA BREADY TO GO!');
-});
-
 // find files that contain .js commands.
 const commandFiles = fs
   .readdirSync('./src/commands')
@@ -27,22 +22,39 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (commandName === 'server') {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
-  } else if (commandName === 'user') {
-    await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
   }
 });
+
+const eventFiles = fs
+  .readdirSync('./src/events')
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.login(token);
